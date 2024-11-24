0d93d086953a76d3a088ba481e6f63da052a6d04
pytholoji <charmyquarkily@gmail.com>
2024-11-24 23:36:59 +0300

    Add files via upload
    ilk soru

---
+++ py.py
@@ -0,0 +1,18 @@
+
+class question1:
+    if __name__=="__main__":
+            
+        _="-"
+        print(f'{_*2:_^4}')
+    """
+    Hangisi doğru çıktıyı gösteriyor?
+        A) --__
+        B) __--
+        C) _--_
+        D) Name Error
+        E) _-_-
+        F) Syntax Error
+    """
+
+
+
