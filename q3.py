28dd0389185569db146e3f84c83fd1107036a1a1
pytholoji <charmyquarkily@gmail.com>
2024-11-25 20:40:35 +0300

    Add files via upload
    3. soru

---
+++ q3.py
@@ -0,0 +1,19 @@
+
+class question3:
+    if __name__ == "__main__":
+
+        palindrom = "ey edip adanada pide ye"
+    """
+    Bu cümleyi tersten nasıl yazdırabiliriz?
+        A) print(palindrom[-1::])
+        B) print(palindrom[:-1:])
+        C) print(reversed(palindrom))
+        D) print(palindrom[::-1])
+    """
+
+    """
+    Cevap: D) print(palindrom[::-1])
+    Çıktı:
+        pass
+    ey edip adanada pide ye
+    """
