c72b29a66605b53bd2a35f73c122b33e221fdfac
pytholoji <charmyquarkily@gmail.com>
2024-11-25 21:04:20 +0300

    Add files via upload
    4. soru

---
+++ q4.py
@@ -0,0 +1,21 @@
+
+class question4:
+    if __name__ == "__main__":
+
+        t = "She's broken because she believed."
+        print(t[1:5],t[8:10],
+              t[13:20],t[22:24],
+              t[27:29]+t[31:33])
+    """
+    Hangisi doğru çıktıyı gösteriyor?
+        A) She's ok becuase she lied.
+        B) He's ok because she lied.
+        C) he's ok because he lied.
+        D) She's broke because he lied.
+    """
+
+    """
+    Çıktı:
+    he's ok because he lied.
+    Cevap: C) he's ok beacuse he lied.
+    """
