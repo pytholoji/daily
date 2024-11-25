6a4871f2269ea1bed46822dd65e7a0d9333abcd4
pytholoji <charmyquarkily@gmail.com>
2024-11-25 20:19:19 +0300

    Add files via upload
    2. soru

---
+++ q2.py
@@ -0,0 +1,25 @@
+
+class question2:
+    if __name__ == "__main__":
+
+        a = 10
+        print( sum( a for a in range(3) ) )
+
+    """
+    Hangisi doğru çıktıyı gösteriyor?
+        A) 30
+        B) 3
+        C) Name Error
+        D) Syntax Error
+        E) 6
+        F) 40
+    """
+
+    """
+    Çıktı:
+    3
+    Cevap: B)
+    """
+
+
+
