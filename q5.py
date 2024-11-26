47064868177cdbe92660d5f97b4cf6ccfa7b27bd
pytholoji <charmyquarkily@gmail.com>
2024-11-26 22:55:02 +0300

    Add files via upload
    5. soru

---
+++ q5.py
@@ -0,0 +1,20 @@
+
+class question5:
+    if __name__ == "__main__":
+
+        a = [5, "2", True, 4.0, 3e0]
+        print(set(map(int, a))):
+
+    """
+    Hangisi doğru çıktıyı veriyor?
+        A) {5, 2, 1, 4, 3}
+        B) (1, 2, 3, 4, 5)
+        C) [5, 2, 1, 4, 3]
+        D) {1, 2, 3, 4, 5}
+    """
+
+    """
+    Çıktı:
+    {1, 2, 3, 4, 5}
+    Cevap: D) {1, 2, 3, 4, 5}
+    """
